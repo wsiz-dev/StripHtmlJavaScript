bee2375eee2bbcf78e3929e919a9dfe9eed92261
Janusz Marcinik <januszmarcinik@gmail.com>
2021-05-29 15:34:06 +0200

    Czwarty przypadek testowy - GREEEN

--- StripHtmlJavaScript.App/HtmlJavaScriptStripper.cs
+++ StripHtmlJavaScript.App/HtmlJavaScriptStripper.cs
@@ -1,12 +1,17 @@
+using System;
+
 namespace StripHtmlJavaScript.App
 {
     public class HtmlJavaScriptStripper
     {
         public string ProcessText(string text)
         {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new ArgumentNullException();
+            }
+
             return text;
         }
-        
-        
     }
 }
